dcdbc96cf91367e5cf35f1a1380681cd65ca5850
Guillermo Ignacio Enriquez Gutierrez <nacho4d@mac.com>
2014-11-08 17:16:02 +0900

    Temporal stuff - Renders a simple list of string - TODO: draw borders, limiters, selection background. - TODO: handle key events

--- gameplay/src/ListBox.cpp
+++ gameplay/src/ListBox.cpp
@@ -41,13 +41,67 @@ void ListBox::initialize(const char* typeName, Theme::Style* style, Properties* 
     Control::initialize(typeName, style, properties);
     if (properties)
     {
-        //_inputMode = getInputMode(properties->getString("inputMode"));
+        // Get all ListItems objects
+        Properties* listItemSpace = properties->getNextNamespace();
+        while (listItemSpace != NULL)
+        {
+            const char* name = listItemSpace->getNamespace();
+            if (strcmp(name, "listItem") == 0)
+            {
+                std::cout << "list item: " << name << std::endl;
+                const char* text = listItemSpace->getString("text");
+                float height = listItemSpace->getFloat("height");
+                ListItem *item = new ListItem(text, height);
+                _items.push_back(item);
+            }
+            else
+            {
+                std::cout << "ListBox expected 'listItem' as a child but '" << name << "' was found" << std::endl;
+            }
+            listItemSpace = properties->getNextNamespace();
+        }
     }
+}
+
+void ListBox::changeSelectedItem(float x, float y)
+{
+    for (int i = 0; i < _items.size(); ++i) {
+        ListItem *item = _items[i];
+        bool itemContainsPosition = false;
+        // TODO: Find if item contains x and y coordinates
+        if (itemContainsPosition) {
+            changeSelectedItem(i);
+            return;
+        }
+    }
+}
+
+void ListBox::changeSelectedItem(int index)
+{
+    // TODO:
+    // Do Validation: 0 <= index < _item.size()
+    // Really change selected index
+    // Change other variables for drawing
+    // Broadcast SELECTED_ITEM_INDEX_CHANGED event
 }
     
 bool ListBox::touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex)
 {
-    return false;
+    if (getState() == ACTIVE) {
+        switch (evt)
+        {
+            case Touch::TOUCH_PRESS:
+                changeSelectedItem(x, y);
+                break;
+            case Touch::TOUCH_MOVE:
+                changeSelectedItem(x, y);
+                break;
+            default:
+                break;
+        }
+    }
+
+    return Control::touchEvent(evt, x, y, contactIndex);
 }
 
 /**
@@ -62,42 +116,94 @@ bool ListBox::touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int conta
  */
 bool ListBox::keyEvent(Keyboard::KeyEvent evt, int key)
 {
-    return false;
+    // TODO: Do better keyboard handling. For example on the PC there is a *transitory* state
+    // which is changed with the arrows keys. The space key makes the transitory item to be selected.
+    // Further more Ctrl (or Command on OSX) allows multi selection
+    switch (evt)
+    {
+        case Keyboard::KEY_PRESS:
+        {
+            switch (key)
+            {
+                case Keyboard::KEY_UP_ARROW:
+                {
+                    changeSelectedItem(_selectedIndex-1);
+                    break;
+                }
+                case Keyboard::KEY_DOWN_ARROW:
+                {
+                    changeSelectedItem(_selectedIndex+1);
+                    break;
+                }
+            }
+        }
+    }
+    return Control::keyEvent(evt, key);
 }
 
-/**
- * @see Control#controlEvent
- */
 void ListBox::controlEvent(Control::Listener::EventType evt)
 {
-    
+    //TODO: Do we need to do something here?
 }
 
-/**
- * @see Control::updateState
- */
 void ListBox::updateState(Control::State state)
 {
-    
+    Control::updateState(state);
+    _font = getFont(state);
+    std::cout << "State " << state << " font: " << _font << std::endl;
 }
 
-/**
- * @see Control::drawImages
- */
 unsigned int ListBox::drawImages(Form* form, const Rectangle& clip)
 {
+    // TODO: draw borders, dividers and highlighted item background
     return 0;
 }
 
-/**
- * @see Control::drawText
- */
 unsigned int ListBox::drawText(Form* form, const Rectangle& clip)
 {
-    return 0;
+    // Draw the text.
+    int drawCalls = 0;
+    Rectangle bounds = _listBounds;
+    for (std::vector<ListItem*>::const_iterator i = _items.begin(); i != _items.end(); ++i) {
+        std::string *text = (*i)->text();
+        float height = (*i)->height();
+        if (text->size() > 0 && _font)
+        {
+            Control::State state = getState();
+            unsigned int fontSize = getFontSize(state);
+            if (height <= 0) {
+                height = fontSize + 2; // add 2 because top margin and bottom margin
+            }
+            bounds.height = height;
+
+            SpriteBatch* batch = _font->getSpriteBatch(fontSize);
+            startBatch(form, batch);
+            _font->drawText(text->c_str(), bounds, _textColor, fontSize, getTextAlignment(state), true, getTextRightToLeft(state), &_viewportClipBounds);
+            finishBatch(form, batch);
+
+            bounds.y += height;
+            ++drawCalls;
+        }
+    }
+
+    return drawCalls;
 }
 
-//    ListBox::ListBox(const ListBox& copy) {}
+void ListBox::updateAbsoluteBounds(const Vector2& offset)
+{
+    Control::updateAbsoluteBounds(offset);
+    _listBounds.set((int)_viewportBounds.x, (int)_viewportBounds.y, _viewportBounds.width, _viewportBounds.height);
+}
+
+void ListBox::update(float elapsedTime)
+{
+    Control::update(elapsedTime);
+
+    // Update text opacity each frame since opacity is updated in Control::update.
+    _textColor = getTextColor(getState());
+    _textColor.w *= _opacity;
+}
+
 
 
 }
